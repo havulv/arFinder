c40ecd834676193de945150ea28d02875d551e69
jandersen7 <johnandersen185@gmail.com>
2016-09-05 16:17:45 -0400

    Added setup and requirements

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name="arFinder",
+    version="1.0",
+    description="A paper search and save",
+    author="John Andersen",
+    author_email="johnandersen185@gmail.com"
+    packages=['arFinder']
+    )
+
