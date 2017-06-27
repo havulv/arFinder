00171f9a37b00806e9d59f6f4b6b7e3c10ea7a5d
John Andersen <johnandersen185@gmail.com>
2017-06-27 07:43:24 +0000

    Changed setup.py to be correct

--- setup.py
+++ setup.py
@@ -3,27 +3,24 @@ import sys
 
 from setuptools import setup, find_packages
 
-SUP_VER = ['3.5']
+SUP_VER = ['3.5', '3.6']
 
 if sys.version_info < (3, 5):
     version = '.'.join([str(x) for x in sys.version_info[:3]])
-    print('Version ' + ' is not supported. Supported versions are ' +
-                    ','.join(SUP_VER))
+    print('Python version ' + ' is not supported. '
+          'Supported versions are ' + ','.join(SUP_VER))
     sys.exit(1)
 
 setup(name="arFinder",
-    version="0.1.0",
-    packages=find_packages(),
-    package_dir={'arFinder' : 'arFinder'},
-    url='https://github.com/jandersen7/arFinder',
-    license='MIT License',
-    description="A paper search and save",
-    author="John Andersen",
-    author_email="johnandersen185@gmail.com",
-    description="arFinder is a package search and save for the arXiv",
-    install_requires=["beautifulSoup4", "requests"],
-    tests_require=["unittest"],
-    keywords=["arxiv", "academic", "papers", "search"]
-    )
-
-
+      version="0.1.0",
+      packages=find_packages(),
+      package_dir={'arFinder': 'arFinder'},
+      url='https://github.com/jandersen7/arFinder',
+      license='MIT License',
+      author="John Andersen",
+      author_email="johnandersen185@gmail.com",
+      description="arFinder is a paper search and save for the arXiv",
+      install_requires=["beautifulSoup4", "requests"],
+      tests_require=["unittest"],
+      keywords=["arxiv", "academic", "papers", "search"]
+      )
