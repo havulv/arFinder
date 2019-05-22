6f54a49c8b12e6f139533ca7205d16e8d434a848
j <johnandersen185@gmail.com>
2019-05-22 19:37:20 -0400

    Creating framework for testing

--- setup.py
+++ setup.py
@@ -1,26 +1,18 @@
-#!/usr/bin/python
-import sys
+#! /usr/bin/env python3.7
 
 from setuptools import setup, find_packages
 
-SUP_VER = ['3.5', '3.6']
-
-if sys.version_info < (3, 5):
-    version = '.'.join([str(x) for x in sys.version_info[:3]])
-    print('Python version ' + ' is not supported. '
-          'Supported versions are ' + ','.join(SUP_VER))
-    sys.exit(1)
-
 setup(name="arFinder",
       version="0.1.0",
-      packages=find_packages(),
+      packages=find_packages(exclude=['*.tests', '*.tests.*',
+                                      'tests.*', 'tests']),
       package_dir={'arFinder': 'arFinder'},
       url='https://github.com/jandersen7/arFinder',
       license='MIT License',
       author="John Andersen",
       author_email="johnandersen185@gmail.com",
       description="arFinder is a paper search and save for the arXiv",
-      install_requires=["beautifulSoup4", "requests"],
-      tests_require=["unittest"],
+      install_requires=["beautifulSoup4>=4.7.1", "requests>=2.21.0"],
+      tests_require=["nose", "parameterized"],
       keywords=["arxiv", "academic", "papers", "search"]
       )
