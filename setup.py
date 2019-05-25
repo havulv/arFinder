f2b820825d5d4f8f532610a60cf6545b0cc2fc97
j <johnandersen185@gmail.com>
2019-05-24 22:41:48 -0400

    Tried to do a soft reset but not working... Setup.py should be the same

--- setup.py
+++ setup.py
@@ -13,6 +13,4 @@ setup(name="arFinder",
       author_email="johnandersen185@gmail.com",
       description="arFinder is a paper search and save for the arXiv",
       install_requires=["beautifulSoup4>=4.7.1", "requests>=2.21.0"],
-      tests_require=["nose", "parameterized"],
-      keywords=["arxiv", "academic", "papers", "search"]
-      )
+      keywords=["arxiv", "academic", "papers", "search"])
